fee327f58cf8d1c2b23c88af194f2ddbf16e3cf0
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-14 20:58:50 -0300

    at

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { api } from "./services/api";
+
+function App() {
+  const [repos, setRepos] = useState([]);
+  const getData = async () => {
+    try {
+      const { data } = await api.get("/repos");
+      setRepos(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(repos);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h1>Meus Repos GitHub</h1>
+      {repos.map((repo) => (
+        <p>{repo.name}</p>
+      ))}
+    </>
+  );
+}
+
+export default App;
